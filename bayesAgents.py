faa7f8eee26f6de80e30ab20e0ba8b185e76354c
Arnav Bhasin <bhasin.arnav07@gmail.com>
2026-03-27 20:38:51 -0500

    q1

--- bayesAgents.py
+++ bayesAgents.py
@@ -96,8 +96,24 @@ def constructBayesNet(gameState):
     edges = []
     variableDomainsDict = {}
 
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    variableDomainsDict[X_POS_VAR] = X_POS_VALS
+    variableDomainsDict[Y_POS_VAR] = Y_POS_VALS
+    variableDomainsDict[FOOD_HOUSE_VAR] = HOUSE_VALS
+    variableDomainsDict[GHOST_HOUSE_VAR] = HOUSE_VALS
+
+    edges.append((X_POS_VAR, FOOD_HOUSE_VAR))
+    edges.append((X_POS_VAR, GHOST_HOUSE_VAR))
+    edges.append((Y_POS_VAR, FOOD_HOUSE_VAR))
+    edges.append((Y_POS_VAR, GHOST_HOUSE_VAR))
+
+    for housePos in gameState.getPossibleHouses():
+        for obsPos in gameState.getHouseWalls(housePos):
+            obsVar = OBS_VAR_TEMPLATE % obsPos
+            if obsVar not in obsVars:
+                obsVars.append(obsVar)
+                variableDomainsDict[obsVar] = OBS_VALS
+                edges.append((FOOD_HOUSE_VAR, obsVar))
+                edges.append((GHOST_HOUSE_VAR, obsVar))
 
     variables = [X_POS_VAR, Y_POS_VAR] + HOUSE_VARS + obsVars
     net = bn.constructEmptyBayesNet(variables, edges, variableDomainsDict)
